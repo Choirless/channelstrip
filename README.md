fea92a4902b65234af765f53d0413d56f93fe231
Glynn Bird <glynn.bird@gmail.com>
2020-09-05 18:23:21 +0100

    complex filter pipeline

---
+++ README.md
@@ -0,0 +1,79 @@
+# channelstrip
+
+An audio manipulation library that behaves like a channel strip on an audio mixing desk
+
+![](channelstrip.png)
+
+It allows the following effects to be applied in this order:
+
+- `trim` - numbers (seconds) - the amount of audio to be trimmed from the start of the file (default: 0)
+- `gate` - boolean - whether to apply a noise gate (default: false)
+- `compress` - boolean - whether to compress the dynamic range (default: false)
+- `volume` - number - volume level: 0=silence 1=unchanged (default: 1)
+- `treble` - number - treble adjustment: -1 to +1 (default: 0)
+- `bass` - number - bass adjustment: -1 to +1 (default: 0)
+- `extrastereo` - boolean - adds stereo effect (default: 0)
+- `pan` - number - stereo pan: -1=left 0=middle +1=right (default: 0)
+- `reverb` - number - amount of reverb effect to apply 0=none 1=lots (default: 0)
+
+## Installing
+
+Add `channelstrip` to your Node.js project:
+
+```sh
+npm install --save channelstrip
+```
+
+> Note: You must have ffmpeg installed on the target system. If you want to install ffmpeg via npm then you can additionally do `npm install --save ffmpeg-static`.
+
+## Using
+
+Include the library in your code:
+
+```js
+const channelstrip = require('channelstrip')
+```
+
+Process an audio file with the `mixdown` function:
+
+```js
+await channelstrip.mixdown({
+  input: 'myaudio.wav',
+  output: 'out.wav',
+  gate: true,
+  compress: true,
+  pan: -0.25,
+  treble: -0.1,
+  bass: 0.8,
+  extrastereo: false,
+  volume: 1.2,
+  trim: 0.1,
+  reverb: 0.2
+})
+```
+
+- `input` - the path, URL or Node.js ReadStream of the input audio
+- `output` - the path, or Node.js WriteStream of the output audio
+
+e.g
+
+```js
+await channelstrip.mixdown({
+  input: 'https://mydomain.com/test.wav',
+  output: 'out.wav',
+  gate: true,
+  reverb: 0.25
+})
+```
+
+Many file formats are supported, with the file extension indicating the file type:
+
+```js
+const rs = fs.createReadStream('./test.ogg')
+await channelstrip.mixdown({
+  input: rs,
+  output: 'out.mp3',
+  volume: 0.6,
+  bass: 1.1
+})
+```
--- index.js
+++ index.js
@@ -1,99 +1,127 @@
 const ffmpeg = require('fluent-ffmpeg')
+const path = require('path')
 
-const processAudio = async (opts) => {
-
+const mixdown = async (opts) => {
   return new Promise((resolve, reject) => {
     const filters = [
     ]
-    
+    let stage = 1
+
     // trim - 0 - inf
     if (opts.trim) {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'atrim',
-        options: { start: opts.trim }
+        options: { start: opts.trim },
+        outputs: [(++stage).toString()]
       })
     }
 
     // noise gate - on or off
     if (opts.gate) {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'agate',
-        options: { mode: 'downward' }
+        options: { mode: 'downward' },
+        outputs: [(++stage).toString()]
       })
     }
 
     // compression - on or off
     if (opts.compress) {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'acompressor',
-        options: { mode: 'downward' }
+        options: { mode: 'downward' },
+        outputs: [(++stage).toString()]
       })
     }
 
     // volume - 0 to 1
     if (typeof opts.volume === 'number') {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'volume',
-        options: { volume: opts.volume }
+        options: { volume: opts.volume },
+        outputs: [(++stage).toString()]
       })
     }
 
     // treble - -1 to +1
     if (opts.treble) {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'treble',
-        options: { gain: opts.treble * 20 }
+        options: { gain: opts.treble * 20 },
+        outputs: [(++stage).toString()]
       })
     }
 
     // bass - -1 to +1
     if (opts.bass) {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'bass',
-        options: { gain: opts.bass * 20 }
+        options: { gain: opts.bass * 20 },
+        outputs: [(++stage).toString()]
       })
     }
 
     // extrastereo - on or off
     if (opts.extrastereo) {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'extrastereo',
-        options: { }
+        options: { },
+        outputs: [(++stage).toString()]
       })
     }
 
     // pan -1 to +1
     if (opts.pan) {
       filters.push({
+        inputs: [stage.toString()],
         filter: 'stereotools',
-        options: { mpan: opts.pan }
+        options: { mpan: opts.pan },
+        outputs: [(++stage).toString()]
+      })
+    }
+
+    // reverb
+    if (opts.reverb) {
+      filters.push({
+        inputs: [stage.toString()],
+        filter: 'asplit',
+        options: { },
+        outputs: ['dry1', 'dry2']
+      })
+      filters.push({
+        inputs: ['dry1', '0'],
+        filter: 'afir',
+        options: { dry: 10, wet: 10 },
+        outputs: ['wet']
+      })
+      filters.push({
+        inputs: ['dry2', 'wet'],
+        filter: 'amix',
+        options: { inputs: 2, weights: '1 ' + opts.reverb },
+        outputs: [(++stage).toString()]
       })
     }
 
     // run the command
-    var command = ffmpeg()
+    ffmpeg()
+      .input(path.join(__dirname, 'hall.wav'))
       .input(opts.input)
-      .audioFilter(filters)
+      .complexFilter(filters, stage.toString())
       .output(opts.output)
+    //      .on('start', console.log)
       .on('error', reject)
-      .on('end',resolve)
+      .on('end', resolve)
       .run()
   })
-
 }
 
-const main = async () => {
-  await processAudio({
-    input: '../magnificent/audio/7.wav',
-    output: './out.wav',
-    gate: false,
-    compress: true,
-    pan: 0,
-    treble: 0,
-    bass: 0.8,
-    extrastereo: false,
-    volume: 1.0,
-    trim: 15
-  })
+module.exports = {
+  mixdown
 }
-main()
